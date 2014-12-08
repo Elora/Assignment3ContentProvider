94543983c25a3141488114bb675fe4c1f1600ca0
Tony Kwan <tonykwan93@gmail.com>
2014-12-08 12:52:23 +0100

    ContentProvider code is the same as in the book. We'll use this for testing purposes. Code works as far as I know, but I have no way for checking this.

---
+++ CompanyXContentProvider/src/kb50/companyxcontent/CompanyProviderActivity.java
@@ -0,0 +1,15 @@
+package kb50.companyxcontent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CompanyProviderActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_company_provider);
+	}
+}
